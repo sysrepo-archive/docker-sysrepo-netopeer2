3fd793140653a350f7a60b4052fac32254247edf
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-09-22 15:53:30 +0200

    remove '--reconfigurable' from the lwaftr script
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- scripts/lwaftr.sh
+++ scripts/lwaftr.sh
@@ -9,7 +9,7 @@ for CPUFREQ in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
    echo -n performance > $CPUFREQ;
 done
 
-$SNABBPATH/snabb lwaftr bench -b /dev/stdout --reconfigurable $SNABBPATH/program/lwaftr/tests/data/icmp_on_fail.conf $SNABBPATH/program/lwaftr/tests/benchdata/ipv{4,6}-0550.pcap
+$SNABBPATH/snabb lwaftr bench -b /dev/stdout $SNABBPATH/program/lwaftr/tests/data/icmp_on_fail.conf $SNABBPATH/program/lwaftr/tests/benchdata/ipv{4,6}-0550.pcap
 #sudo $SNABBPATH/snabb lwaftr run --cpu 12 --ring-buffer-size 1024 \
 # --conf $SNABBCONFPATH/lwaftrhconly.conf --reconfigurable \
 # --on-a-stick 0000:27:00.1 | tee /var/log/snabb.log
